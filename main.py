4d2f578cb713be82131309d69d56438c38d7edf8
FangFang <fang6ang@outlook.com>
2022-02-08 22:25:06 +0800

    Finish auto fill up

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from utils import mani, gen_bro
+from time import sleep
+from selenium.common.exceptions import NoSuchElementException
 
 url = 'https://portal.pku.edu.cn/'
 user_agent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36'
@@ -14,16 +16,31 @@ def main(dvr_root=r'./chromedriver'):
     bro.get(url)
 
     # Loggin portal
-    id_input = bro.find_element_by_xpath('')
+    try:
+        bro.find_element_by_xpath('//*[@id="ng-app"]/div[1]/header/section/section[1]/section[1]/ul[1]/li/a').click()
+    except NoSuchElementException: # The page's been auto relocated
+        sleep(2)
+
+    id_input = bro.find_element_by_id('user_name')
     id_input.send_keys(account['id'])
-    pass_input = bro.find_element_by_xpath('')
+    pass_input = bro.find_element_by_id('password')
     pass_input.send_keys(account['pass'])
-    log_bttn = bro.find_element_by_xpath('')
-    log_bttn.click()
+    bro.find_element_by_id('logon_button').click()
+    sleep(3)
 
-    zhanyi_bttn = bro.find_element_by_xpath('')
-    zhanyi_bttn.click()
+    # try:
+    #     btn = bro.find_element_by_xpath('//*[@id="bizTip"]/div/div/div[1]/div/div/table/tbody/tr[11]/td/a')
+    #     print('Here')
+    #     btn.click()
+    #     print('click')
+    # except NoSuchElementException:
+    #     pass
 
+    bro.refresh()
+    sleep(3)
+    bro.find_element_by_id('all').click()
+    bro.find_element_by_id('fav_epidemic').click()
+    sleep(10)
     mani(bro)
 
 

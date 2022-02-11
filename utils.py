ecd306f9272f5a99b9477b3d983c84220d65e677
qinyifang <fang6ang@outlook.com>
2022-02-11 11:18:34 +0800

    Add args

--- utils.py
+++ utils.py
@@ -6,6 +6,34 @@ DAILY_INFO_FORM = '''
 app.dailyInfoForm.sfdrfj = "n";
 app.dailyInfoForm.sfczzz = "n";
 app.dailyInfoForm.yqzd = "健康";
+app.dailyInfoForm.jrtw = 36.3;
+'''
+
+# 所有选项
+'''
+app.dailyInfoForm.sfhx = "n";                 // 是否返校
+app.dailyInfoForm.hxsj = "20200901 120000";   // 返校时间
+app.dailyInfoForm.cfdssm = "11";              // 出发地所在省
+app.dailyInfoForm.cfddjsm = "01";             // 出发地所在市
+app.dailyInfoForm.cfdxjsm = "08";             // 出发地所在区/县
+app.dailyInfoForm.dqszdsm = "11";             // 当前所在省
+app.dailyInfoForm.dqszddjsm = "01";           // 当前所在市
+app.dailyInfoForm.dqszdxjsm = "08";           // 当前所在区/县
+app.dailyInfoForm.dqszdgbm = "";              // 当前所在国家
+app.dailyInfoForm.dqszdxxdz = "No.5 YHY Rd."; // 当前所在地详细地址
+app.dailyInfoForm.sfdrfj = "n";               // 是否当日返京
+app.dailyInfoForm.chdfj = "Shin Nippori St."; // 从何地返京
+app.dailyInfoForm.sflsss = "n";               // 当日是否留宿宿舍
+app.dailyInfoForm.sfcx = "n";                 // 是否出校
+app.dailyInfoForm.sfmjqzbl = "n";             // 是否与确诊病例密接
+app.dailyInfoForm.sfmjmjz = "n";              // 是否与确诊病例密接者密接
+app.dailyInfoForm.hsjcjg = "";                // 核酸检测结果
+app.dailyInfoForm.jjgcsj = "";                // 开始居家健康观察的时间
+app.dailyInfoForm.sfzgfxdq = "n";             // 是否居住在中高风险地区
+app.dailyInfoForm.jrtw = 37;                  // 今日体温
+app.dailyInfoForm.sfczzz = "n";               // 是否存在以下症状
+app.dailyInfoForm.yqzd = "健康";               // 疫情诊断
+app.dailyInfoForm.jqxdgj = "";                // 行动轨迹
 '''
 
 def gen_bro(root) -> webdriver.Chrome:
@@ -26,6 +54,7 @@ def mani(bro: webdriver.Chrome):
     sleep(2)
 
     bro.execute_script(DAILY_INFO_FORM)
+    sleep(2)
     bro.execute_script('app.saveMrtb()')
     sleep(2)
     bro.refresh()
